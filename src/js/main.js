5c2d8c1a089b10ce7fc1259151fe47d0a41334eb
Emil Olejnik <olejnike2000@gmail.com>
2022-04-13 17:01:55 +0200

    Minor fixes; challenge completed!

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const pledgeModal = document.querySelector('.modal__back');
 const modalShadow = document.querySelector('.modal__shadow');
 const allRewardBtns = document.querySelectorAll('.card--pledge .card__button');
 const modalClose = pledgeModal.querySelector('.modal__close');
-
+const bookmarkBtns = document.querySelectorAll('.card__buttons-box button');
 let modalCards = Array.from(pledgeModal.querySelectorAll('.card'));
 //Show and close modals and menu
 const mobileMenuHandler = () => {
@@ -54,8 +54,44 @@ function changeFocus() {
     }
     previos = this;
 }
+let flag = false;
+const changeSrc = (e) => {
+    console.log(e.target);
+
+    if(e.target.classList.contains('bookmark-ico-mobile') && !flag)
+    {
+        e.target.setAttribute('src', './dist/img/icon-bookmark-marked.svg');
+        flag = !flag
+    }
+    else
+    {
+        e.target.setAttribute('src', './dist/img/icon-bookmark.svg');
+        flag = !flag;
+    }
+    if(e.target.closest('.card__button').classList.contains('card__button--bookmark--desktop') && !flag)
+    {
+        e.target.closest('.card__button').firstChild.setAttribute('src', './dist/img/icon-bookmark-marked.svg');
+        e.target.closest('.card__button').lastChild.style.color = 'hsl(176, 50%, 47%)';
+        e.target.closest('.card__button').lastChild.textContent = 'Bookmarked!';
+        flag = !flag;
+    }
+    else
+    {
+        e.target.closest('.card__button').firstChild.setAttribute('src', './dist/img/icon-bookmark.svg');
+        e.target.closest('.card__button').lastChild.style.color = 'hsl(0, 0%, 48%)';
+        e.target.closest('.card__button').lastChild.textContent = 'Bookmark';
+        flag = !flag;
+    }
+}
+bookmarkBtns.forEach(btn =>
+    {
+        btn.addEventListener('click', changeSrc);
+    })
+
+
 window.addEventListener('resize', ()=>
 {
+    if(previos == null) return;
     window.innerWidth >= 768 ? previos.querySelector('.card__pledge-manager').style.display = 'flex' : previos.querySelector('.card__pledge-manager').style.display = 'block'
 })
 modalCards = filterCards();
